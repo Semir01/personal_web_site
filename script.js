1e16565f190428f044e86cfd2983c2c9a3cb51e2
semir <semirjazvin3@gmail.com>
2025-10-30 11:22:08 +0100

    Changed some details and formating the code

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+/*  ============================ Event Listeners ============================  */
 document.addEventListener("DOMContentLoaded", () => {
+   LoadingScreen();
+
+});
+
+
+/*  ============================ Functions ============================  */
+
+function LoadingScreen() {
     const loadingScreen = document.getElementById("loading-screen");
     loadingScreen.classList.remove("hidden");
 
     setTimeout(() => {
-      loadingScreen.classList.add("hidden");
-    }, 2000); 
-  });
+        loadingScreen.classList.add("hidden");
+    }, 2000);
+}
